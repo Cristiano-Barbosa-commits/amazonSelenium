e2f78eec9021fa8ea7e8571353dde6b0bd95795f
CristianoBarbosa <86861536+Cristiano-Barbosa-commits@users.noreply.github.com>
2023-07-19 10:22:21 -0300

    Update paginaAmazon.java

--- paginaAmazon.java
+++ paginaAmazon.java
@@ -1,14 +1,27 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class testeDoSiteAmazon {
 
-	@Test
-	public void amazonTest() {
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-		WebDriver browser = new ChromeDriver();
-		browser.navigate().to("https://amazon.com.br");
+public class testeDoSiteAmazon {
+	private static final String URL_LOGIN = "http://www.amazon.com.br";
+	private WebDriver browser;
+
+
+	@BeforeAll
+	public static void beforeAll() {
+	System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+	}
+
+	@BeforeEach
+	public void beforeEach() {
+	this.browser = new ChromeDriver();
 	}
 
 }
