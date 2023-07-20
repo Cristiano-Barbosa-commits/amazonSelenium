520fb900f38ca1e6c6af2f0b04665d35da68fdc5
CristianoBarbosa <86861536+Cristiano-Barbosa-commits@users.noreply.github.com>
2023-07-20 20:37:10 -0300

    Update produtoPage.java
    criando arquivo de page objects

--- produtoPage.java
+++ produtoPage.java
@@ -1,1 +1,24 @@
+import org.openqa.selenium.chrome.ChromeDriver;
 
+pubic class produtoPage {
+  
+  private static final String URL_AMAZON = "http://www.amazon.com.br";
+	private WebDriver browser;
+
+  public produtosPage () {
+	System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+	this.browser = new ChromeDriver();
+  this.browser.navigate().to(URL_AMAZON);
+
+  public void navegarAteProdutos () {
+    browser.navigate().to(URL_AMAZON);
+		browser.findElement(By.id("twotabsearchtextbox")).sendKeys("Playstation 5");
+  }
+
+  public void clicarEmBuscar () {
+    browser.findElement(By.id("nav-search-submit-button")).click();
+
+  }
+  public void fechar()
+    this.bowser.quit();
+	}
